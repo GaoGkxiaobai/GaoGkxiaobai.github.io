75fe1c7755336269be4fd7be5e9446d38f8f7d98
zcjgiteezc <2736104855@qq.com>
2019-12-21 17:31:41 +0800

    index change

--- src/components/Headnav.vue
+++ src/components/Headnav.vue
@@ -25,13 +25,15 @@ ul{
         margin: 0.07rem 0.125rem;
         font: 0.14rem/0.2rem "宋体";
         color: #72727b;
+        text-align: center;
+        padding-bottom:0.13rem;
     }
 }
 .active{
     width: 0.52rem;
     height: 0.2rem;
     margin: 0.07rem 0.125rem;
-    font: 0.14rem/0.2rem "宋体";
+    font: 0.18rem/0.2rem "宋体";
     color: #40404c;
     border-bottom: 3px solid red;
 }
--- src/views/index/Recommend.vue
+++ src/views/index/Recommend.vue
@@ -1,5 +1,6 @@
 <template>
 <indexcomponent></indexcomponent>
+
 </template>
 <script>
 import indexcomponent from '@/components/Indexcomponet'
