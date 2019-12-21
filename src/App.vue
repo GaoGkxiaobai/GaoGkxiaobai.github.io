a693063ba3f4c645f9644d4ea1e1d534b640626f
ggk <1119882054@qq.com>
2019-12-21 11:01:28 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,69 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="main">
+    <!-- {{aname}} -->
+    <!-- <div class="chengshi" @click=""> -->
+
+      <!-- 城市 -->
+    <!-- </div> -->
+    <tabber v-if="isShow"></tabber>
+    <!-- <tabber v-if="$store.state.isShow"></tabber> -->
+    <!-- 路由容器 -->
+
+    <router-view></router-view>
+    <!-- <narban name="my name is narban"></narban> -->
+    <!-- <maizuo></maizuo> -->
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import narban from './components/narban'
+// import maizuo from './components/maizuo'
+import tabber from './components/Tabbar'
+import { mapState } from 'vuex'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  data () {
+    return {
+      aname: 'ggk'
     }
+  },
+  computed: {
+    ...mapState(['isShow'])
+  },
+  components: {
+    // narban
+    // maizuo,
+    tabber
+  },
+  methods: {
+
   }
 }
+</script>
+<style lang="scss" scoped>
+  // #main{
+  //   height: 200px;
+  //   width: 200px;
+  //   background-color: red;
+  //   color: white;
+  // }
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  ul,ol,li{
+    list-style: none;
+  }
+
+  html,body{
+    height: 100%;
+
+  }
+  #main{
+    // height: 100%;
+    // position: relative;
+    // padding-bottom: 50px;
+    // margin: 0;
+    // padding: 0;
+  }
 </style>
